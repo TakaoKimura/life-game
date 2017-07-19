04404a48e6b7ce15c29fb81c9c723e91c98b638f
ShoichiroKitano <shoichiro.kitano@gaiax.com>
2017-07-19 21:03:32 +0900

    テストリファクタ

--- test/unit/ResultTest.java
+++ test/unit/ResultTest.java
@@ -5,50 +5,54 @@ import static org.junit.Assert.assertTrue;
 import static org.hamcrest.core.Is.is;
 
 public class ResultTest {
+
+  private Cell cell;
+  private Result result;
+
+  private void setUp(boolean state, int countOfArrive) {
+    cell = new Cell(0, 0, state);
+    result = new Result(cell, countOfArrive);
+  }
+
   @Test
   public void カウントが0なら死ぬ() {
-    Cell cell = new Cell(0, 0, true);
-    Result result = new Result(cell, 0);
+    setUp(true, 0);
     result.apply();
     assertFalse(cell.getState());
   }
 
   @Test
   public void カウントが1なら死ぬ() {
-    Cell cell = new Cell(0, 0, true);
-    Result result = new Result(cell, 1);
+    setUp(true, 1);
     result.apply();
     assertFalse(cell.getState());
   }
 
   @Test
   public void カウントが2なら維持する() {
-    Cell cell = new Cell(0, 0, false);
-    Result result = new Result(cell, 2);
+    setUp(false, 2);
+    result.apply();
     result.apply();
     assertFalse(cell.getState());
   }
 
   @Test
   public void カウントが3なら誕生する() {
-    Cell cell = new Cell(0, 0, false);
-    Result result = new Result(cell, 3);
+    setUp(false, 3);
     result.apply();
     assertTrue(cell.getState());
   }
 
   @Test
   public void カウントが4なら死ぬ() {
-    Cell cell = new Cell(0, 0, true);
-    Result result = new Result(cell, 4);
+    setUp(true, 4);
     result.apply();
     assertFalse(cell.getState());
   }
 
   @Test
   public void カウントが4以上なら死ぬ() {
-    Cell cell = new Cell(0, 0, true);
-    Result result = new Result(cell, 5);
+    setUp(true, 5);
     result.apply();
     assertFalse(cell.getState());
   }
