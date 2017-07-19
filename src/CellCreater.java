c53a9652e4b120e9cf5b64516e96e0cdd003b51d
ShoichiroKitano <shoichiro.kitano@gaiax.com>
2017-07-19 21:39:12 +0900

    セルの生成を移譲

---
+++ src/CellCreater.java
@@ -0,0 +1,12 @@
+class CellCreater {
+
+  public static Cells create(int width, int height) {
+    Cells cells = new Cells();
+    for(int x = 1; x <= width; x++) {
+      for(int y = 1; y <= height; y++) {
+        cells.add(new Cell(x, y, false));
+      }
+    }
+    return cells;
+  }
+}
---
+++ test/unit/CellCreaterTest.java
@@ -0,0 +1,31 @@
+import org.junit.Test;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.hamcrest.core.Is.is;
+
+public class CellCreaterTest {
+
+  public void 起点から横にxこ縦にyこのセルを生成する() {
+    Cells cells = CellCreater.create(2, 2);
+    assertNotNull(cells.at(1, 1));
+    assertNotNull(cells.at(2, 1));
+    assertNull(cells.at(3, 1));
+    assertNotNull(cells.at(1, 2));
+    assertNotNull(cells.at(2, 2));
+    assertNull(cells.at(3, 2));
+    assertNull(cells.at(1, 3));
+    assertNull(cells.at(2, 3));
+    assertNull(cells.at(3, 3));
+  }
+
+  public void デフォルトは全てのCellが死んでいる() {
+    Cells cells = CellCreater.create(2, 2);
+    assertFalse(cells.at(1, 1).isAlive());
+    assertFalse(cells.at(2, 1).isAlive());
+    assertFalse(cells.at(1, 2).isAlive());
+    assertFalse(cells.at(2, 2).isAlive());
+  }
+
+}
